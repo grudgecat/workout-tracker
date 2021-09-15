a5590bcd2ea1ce6f145e563f0222a3a3b91ef31d
grudgecat <grudgecat@protonmail.com>
2021-09-15 13:26:34 -0700

    update to server.js for heroku connection

--- server.js
+++ server.js
@@ -19,7 +19,9 @@ app.use(routes);
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+  useCreateIndex: true
 });
 
 app.listen(PORT, () => {
